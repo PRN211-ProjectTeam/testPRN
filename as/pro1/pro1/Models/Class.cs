863068640313227cf6550bd5c10fc72b667e38d6
kurzii-0903 <quylai62@gmail.com>
2023-10-19 22:45:27 +0700

    .

---
+++ as/pro1/pro1/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace pro1.Models
+{
+    public class Class
+    {
+        public Class()
+        {
+            ClassSubjects = new HashSet<ClassSubject>();
+        }
+
+        public int ClassId { get; set; }
+        public string? ClassName { get; set; }
+
+        public virtual ICollection<ClassSubject> ClassSubjects { get; set; }
+    }
+}
