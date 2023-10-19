2e90f640cbed7e94afb4df5f6e46f32cc9b6f07b
kurzii-0903 <quylai62@gmail.com>
2023-10-19 22:38:05 +0700

    .

---
+++ as/pro1/pro1/Models/Pending.cs
@@ -0,0 +1,10 @@
+﻿namespace pro1.Models
+{
+    public class Pending
+    {
+        public int MoveId { get; set; }
+        public int? StudentId { get; set; }
+        public int? ClassSubjectId { get; set; }
+        public bool? Status { get; set; }
+    }
+}
