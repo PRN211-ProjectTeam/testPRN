4ff37f3246117565634e34e3a398eb4607421d0f
kurzii-0903 <quylai62@gmail.com>
2023-10-27 12:18:04 +0700

    27.10

---
+++ project/swp391_project-main/PetShop_Project_SWP391/Models/PictureProduct1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace PetShop_Project_SWP391.Models
+{
+    public partial class PictureProduct1
+    {
+        public PictureProduct1()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int PictureId { get; set; }
+        public string? Picture { get; set; }
+        [JsonIgnore]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
